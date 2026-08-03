3172e6cf3dc5c72592ff8cec5335563de93a1306
pkobelka <p.kobelka@gmail.com>
2026-08-03 13:12:46 +0200

    QR přihlášení tabletu dostupné i z menu (#19)
    Nové tlačítko v menu 'Přihlásit tablet (QR)' otevře okno s generátorem QR (pošli odkaz -> vlož -> QR), takže QR jde vyrobit i když jsi přihlášený. Lokální generování, beze změny bezpečnosti. Bump v1.175, CACHE florian-v182.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Florián – samostatný service worker pro /florian/
 // Účel: vlastní scope (oddělení od dalších PWA) a instalace na plochu.
-const CACHE = 'florian-v181';
+const CACHE = 'florian-v182';
 const ASSETS = [
   './',
   './index.html',
