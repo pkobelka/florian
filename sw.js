c5bf51d8d75c8e947a00bf384f2749f40224d288
pkobelka <p.kobelka@gmail.com>
2026-08-03 14:44:07 +0200

    Tablet: ovládání pod hamburger (tlačítko polohy bylo překryté) (#24)
    Práh hamburgeru 640 -> 1024 px (CSS + JS), aby se na tabletu ovládací panel sbalil a nepřekrýval tlačítko Moje poloha. Bump 1.180, CACHE florian-v187.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Florián – samostatný service worker pro /florian/
 // Účel: vlastní scope (oddělení od dalších PWA) a instalace na plochu.
-const CACHE = 'florian-v186';
+const CACHE = 'florian-v187';
 const ASSETS = [
   './',
   './index.html',
